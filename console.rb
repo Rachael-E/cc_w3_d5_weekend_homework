982f174e13afab748efa989cd4bc7ca43466b0f0
Rachael <rachael.ellen@btopenworld.com>
2018-03-23 14:57:52 +0000

    initializing weekend homework repo

---
+++ console.rb
@@ -0,0 +1,34 @@
+require("pry-byebug")
+require_relative("models/customer.rb")
+require_relative("models/film.rb")
+require_relative("models/ticket.rb")
+###########################################
+Ticket.delete_all()
+Film.delete_all()
+Customer.delete_all()
+###########################################
+film1 = Film.new({ 'title' => 'Coco', 'price' => '5' })
+film2 = Film.new({ 'title' => 'Black Panther', 'price' => '8'})
+film3 = Film.new({ 'title' => 'Thor Ragnarok', 'price' => '10'})
+film1.save
+film2.save
+film3.save
+film2.delete
+############################################
+customer1 = Customer.new({ 'name' => 'Rachael', 'funds' => '50' })
+customer2 = Customer.new({ 'name' => 'Gordon', 'funds' => '50' })
+customer3 = Customer.new({ 'name' => 'Rebecca', 'funds' => '20' })
+customer1.save
+customer2.save
+customer3.save
+###########################################
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film1.id})
+ticket3 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film3.id})
+ticket4 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film3.id})
+ticket1.save
+ticket2.save
+ticket3.save
+##########################################
+
+p Film.all
